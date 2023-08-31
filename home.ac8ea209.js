e5fd455306c8a7978636791a46f3af92463ce8c8
RavellRavindo <92678986+RavellRavindo@users.noreply.github.com>
2023-09-01 02:23:55 +0700

    Add files via upload

---
+++ home.ac8ea209.js
@@ -0,0 +1,7 @@
+!async function(){let t=await fetch("dataContent.json"),n=await t.json(),e=document.getElementById("bookList");n.forEach(t=>{e.innerHTML=e.innerHTML+`<tr>
+             <td>${t.title}</td>
+             <td>${t.author}</td>
+        </tr>
+        `})}();//# sourceMappingURL=home.ac8ea209.js.map
+
+//# sourceMappingURL=home.ac8ea209.js.map
